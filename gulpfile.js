5d50c4111672aa4f852308d92c7dac67ac8c4764
sean <seanbrwh@gmail.com>
2021-09-07 15:33:25 -0600

    adding basic folder creation

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,31 @@
-const { src, dest } = require("gulp");
+const { src, dest, series } = require("gulp");
+const fs = require("fs");
 
-exports.default = function () {
+function checkViewDir() {
   return src("./src/server/views/index.ejs").pipe(dest("./dist/server/views"));
-};
+}
+
+function ensureDBDirExsists(cb) {
+  ensureExists("./dist/mongo_data", 0744, function (err) {
+    if (err) {
+      console.error({ folderCreationError: err });
+    } else {
+      cb();
+    }
+  });
+}
+
+exports.default = series(checkViewDir, ensureDBDirExsists);
+
+function ensureExists(path, mask, cb) {
+  if (typeof mask == "function") {
+    cb = mask;
+    mask = 0777;
+  }
+  fs.mkdir(path, mask, function (err) {
+    if (err) {
+      if (err.code == "EEXIST") cb(null);
+      else cb(err);
+    } else cb(null);
+  });
+}
